d78d923e639ec7f4d02bb0894c126dd9eaa1871d
Pauloamm <59803151+Pauloamm@users.noreply.github.com>
2020-10-12 14:01:27 +0100

    Flshlight working
    Flashight turns on ad off and recharges according to rpess or hold F, althou it need the lerp after camera

--- Assets/Scripts/Flashlight/Flashlight.cs
+++ Assets/Scripts/Flashlight/Flashlight.cs
@@ -10,6 +10,11 @@ public class Flashlight : MonoBehaviour, IRechargeable
     [SerializeField]
     private Light flashlightLight;
 
+    [SerializeField]
+    Transform player, playerCamera;
+
+    private Vector3 offset;
+
     public MovementInfo info;
 
 
@@ -25,9 +30,17 @@ public class Flashlight : MonoBehaviour, IRechargeable
 
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         if (flashlightLight == null) flashlightLight = this.GetComponentInChildren<Light>();
+
+        GameObject playerObject = GameObject.FindGameObjectWithTag("Player");
+
+        player = playerObject.transform;
+        playerCamera = playerObject.GetComponentInChildren<Camera>().transform;
+
+        offset = playerCamera.position - this.transform.position;
+
 
         RemainingCharges = 0;
         isRecharging = false;
@@ -37,6 +50,10 @@ public class Flashlight : MonoBehaviour, IRechargeable
     // Update is called once per frame
     void Update()
     {
+
+        UpdatePosition();
+        //MouseFollow();
+
 
         // If it is only a click
         if (Input.GetKeyUp(KeyCode.F))
@@ -70,7 +87,6 @@ public class Flashlight : MonoBehaviour, IRechargeable
                 currentTimer += Time.deltaTime;
         }
 
-        //MouseFollow();
 
 
 
@@ -80,7 +96,7 @@ public class Flashlight : MonoBehaviour, IRechargeable
 
     public void Recharge()
     {
-        
+
     }
 
     public void MouseFollow()
@@ -95,7 +111,7 @@ public class Flashlight : MonoBehaviour, IRechargeable
         // Read Mouse Movement
         Vector2 mouseXY = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
 
-        float nextAngular ;
+        float nextAngular;
 
         float mousesens = 20f;
         // Gets mouse inputs
@@ -117,11 +133,31 @@ public class Flashlight : MonoBehaviour, IRechargeable
         info.orientationV2.y = Mathf.Clamp(info.orientationV2.y, -Mathf.PI / 2, Mathf.PI / 2);
 
         // Resets rotations for next frame(values are additive and odnt reset after update)
-        flashlightLight.transform.rotation = Quaternion.identity;
+        /*flashlightLight.*/transform.rotation = Quaternion.identity;
 
-        // Rotates all player right-left
-        flashlightLight.transform.Rotate(flashlightLight.transform.up, info.orientationV2.x * Mathf.Rad2Deg);
+        // Rotates flashlight right-left
+        /*flashlightLight.*/transform.Rotate(flashlightLight.transform.up, info.orientationV2.x * Mathf.Rad2Deg,Space.World);
+        /*flashlightLight.*/transform.Rotate(flashlightLight.transform.right, info.orientationV2.y * Mathf.Rad2Deg, Space.World);
+
     }
 
+    public void UpdatePosition()
+    {
+        //this.transform.position = player.position + offset;
+
+
+        //this.transform.rotation = Quaternion.Slerp(this.transform.rotation, playerCamera.transform.rotation + offset, 3.0f * Time.deltaTime);
+
+        
+        this.transform.forward = Vector3.Lerp(player.transform.forward , player.transform.forward + offset, 3.0f * Time.deltaTime);
+        
+        
+    }
+
+    //IEnumerator Offset()
+    //{
+    //    yield return new WaitForSeconds(0.3f);
+    //    this.transform.
+    //}
 
 }
