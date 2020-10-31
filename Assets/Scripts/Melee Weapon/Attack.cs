d8a07d9e9f5d4f52bc1e02f1dc2dd0805cff6bb3
fabiodanil2 <a17007@alunos.ipca.pt>
2020-10-31 15:08:40 +0000

    melee wepond done with events

--- Assets/Scripts/Melee Weapon/Attack.cs
+++ Assets/Scripts/Melee Weapon/Attack.cs
@@ -4,19 +4,26 @@ using UnityEngine;
 
 public class Attack : MonoBehaviour
 {
+    public delegate void OnAttack();
+    public OnAttack Attacking;
+
     [SerializeField]
     Animator anim;
     [SerializeField]
     GameObject bat;
 
+    public void Start()
+    {
+         Attacking += Func; 
+    }
     private void FixedUpdate()
     {
-        if(Input.GetKeyDown(KeyCode.Alpha3))
+        if (Input.GetKeyDown(KeyCode.Alpha3))
         {
             bat.SetActive(true);
         }
 
-        if(Input.GetKeyDown(KeyCode.Alpha0))
+        if (Input.GetKeyDown(KeyCode.Alpha0))
         {
             bat.SetActive(false);
         }
@@ -24,14 +31,19 @@ public class Attack : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (bat.activeSelf==true)
+        Attacking?.Invoke();
+      
+    }
+
+    private void Func()
+    {
+        if (bat.activeSelf == true)
         {
-           
+
             if (Input.GetMouseButtonDown(0))
                 anim.SetBool("Attacking", true);
             if (Input.GetMouseButtonUp(0))
                 anim.SetBool("Attacking", false);
         }
-        
     }
 }
