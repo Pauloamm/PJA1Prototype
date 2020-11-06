5cd7e45031cd04d3943e74d0af7f0d78bfb54fd7
Pauloamm <59803151+Pauloamm@users.noreply.github.com>
2020-11-06 19:53:02 +0000

    WeaponHierarchyPLZWORKFFS
    Hierarchy weapon working

---
+++ Assets/Scripts/Gun/WeaponHierarchy/Weapon.cs
@@ -0,0 +1,234 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public  class Weapon : Storable
+{
+   
+    [SerializeField]
+    public int BulletsinCurrentMagazine = 10;        // Bullets available in this gun
+    [SerializeField]
+    public int DefaultMagazineSize = 10;   // Size of the magazine
+    [SerializeField]
+    public int BulletDamage = 1;    // Set the number of hitpoints that each pellet will take away from shot objects with a health script
+    [SerializeField]
+    public float defaultShotCooldown = 0.25f;  // Number in seconds which controls how often the player can fire
+    [SerializeField]
+    public float recoil = 20.0f;    // Recoil angle after shooting
+    [SerializeField]
+    public float WeaponKickRecoil = 0.15f;  // Kickback intensity after shooting
+    [SerializeField]
+    public float pelletSpreadRadiusMultiplier = 1.0f;   // Maximum spread radius per pellet (multiplier)
+    [SerializeField]
+    public int pelletsPerBulletShot = 5;  // Number of pellets per bullet shoot
+    [SerializeField]
+    public float gunRange = 50f; // Distance in Unity units over which the player can fire
+    [SerializeField]
+    public float hitForce = 100f;   // Amount of force which will be added to objects with a rigidbody shot by the player
+    [SerializeField]
+    public AudioClip[] gunSounds;
+
+    [SerializeField]
+    public float nextShotCooldown; // Float to store the time left until the player will be allowed to fire again, after firing
+
+    [SerializeField]
+    protected Camera fpsCam;          // Holds a reference to the first person camera
+
+    //RayCastOrigi
+    public Vector3 rayOrigin;
+
+    [SerializeField]
+    private AudioSource gunAudio;   // Holds a reference to the audio source which will play our shooting and reloading sound effects
+
+
+    public delegate void OnShooting(int bullets);
+    public event OnShooting Shot;
+
+    [SerializeField]
+    private PelletHoleManager pelletHoleManager;
+
+    // Default position and rotation for recoil animation
+    private Vector3 defaultLocalPosition;
+    private Quaternion defaultLocalRotation;
+
+
+    // Keycode associated to weapon for fast equiping
+    [SerializeField] public KeyCode weaponKeyCode;
+    
+    // Equiping manager
+    [SerializeField] private WeaponManager weaponManager;
+
+    private void Awake()
+    {
+        pelletHoleManager = new PelletHoleManager();
+    }
+    private void Update()
+    {
+        // Gradually restore position and rotation after shooting kickback and recoil, respectively
+        this.transform.localPosition = Vector3.Lerp(this.transform.localPosition, defaultLocalPosition, Time.deltaTime * 4f);
+        this.transform.localRotation = Quaternion.Lerp(this.transform.localRotation, defaultLocalRotation, Time.deltaTime * 4f);
+
+        // If there is a delay for the next shot
+        if (nextShotCooldown > 0)
+        {
+            // Keep decreasing the delay
+            nextShotCooldown -= Time.deltaTime;
+        }
+    }
+    public virtual void Attacking()
+    {
+
+        // Check if enough time has elapsed since they last fired and if there is at least 1 bullet available
+        if (nextShotCooldown > 0 || BulletsinCurrentMagazine <= 0) return;
+
+        // Update the time when our player can fire next
+        nextShotCooldown = defaultShotCooldown;
+
+        PlayShootingSound();
+
+        // Create a vector at the center of our camera's viewport
+        rayOrigin = fpsCam.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0.0f));
+
+        // Reduce the bullets available
+        BulletsinCurrentMagazine--;
+
+        // Declare a raycast hit to store information about what our raycast has hit
+        RaycastHit hit;
+
+        Vector3 pelletDirection = WeaponSpread();
+
+        // Check if our raycast has hit anything
+        if (Physics.Raycast(rayOrigin, pelletDirection, out hit, gunRange))
+        {
+
+            // Get a reference to an Enemy script attached to the collider we hit
+            Enemy enemy = hit.collider.GetComponent<Enemy>();
+
+            float impactPercentageWithDistance = WeaponForceCalculation(hit);
+
+            DoDamage(enemy, impactPercentageWithDistance);
+
+            ApplyWeaponForce(hit, impactPercentageWithDistance);
+
+            HoleCreation(hit);
+            
+            
+
+        }
+
+        WeaponRecoil();
+        Debug.Log($"bullets: {BulletsinCurrentMagazine}");
+        ShotEvent();
+    }
+    
+    
+    
+    //CReates the holes
+    protected void HoleCreation(RaycastHit hit) => pelletHoleManager.NewPelletHole(hit.point, hit.collider.gameObject);
+
+    //Invokes the shot Event
+    protected void ShotEvent()
+    {
+        Shot?.Invoke(BulletsinCurrentMagazine);
+    }
+
+    //Responsable for the weaponRecoil
+    protected void WeaponRecoil()
+    {
+        this.transform.localRotation = defaultLocalRotation;
+        this.transform.localPosition = defaultLocalPosition;
+        this.transform.localPosition -= this.transform.forward * WeaponKickRecoil;
+        this.transform.Rotate(-recoil, 0f, 0f);
+    }
+
+    //OnPickUp overrrides the default values
+    public void OnPickUpDefaultInit(Quaternion localRotation, Vector3 localPosition)
+    {
+        defaultLocalRotation = localRotation;
+        defaultLocalPosition = localPosition;
+    }
+    
+   
+    //Changes the magazine for the current weapon with a magazine thats has random bullets in her
+
+    public void ChangeMagazine(int spareMagazines)
+    {
+        if(spareMagazines>0)
+        {
+            float fillPercentage = RandomNonLinearProbabilityPercentage();
+            BulletsinCurrentMagazine = (int)(DefaultMagazineSize * fillPercentage);
+            // Play the reloading sound effect
+            gunAudio.PlayOneShot(gunSounds[1]);
+        }
+
+    }
+
+    //Calculates the % of next magazines bullets 
+    private float RandomNonLinearProbabilityPercentage()
+    {
+        float randNonLinearProbabilityPercentage = 0.00f;
+
+        for (int i = 1; i <= 4; i++)
+        {
+            randNonLinearProbabilityPercentage = UnityEngine.Random.Range(randNonLinearProbabilityPercentage, 0.25f * i);
+        }
+
+        return randNonLinearProbabilityPercentage;
+    }
+    
+
+    //Stores the weapon on the inventory
+    public override void StoreItem()
+    {
+        slotManager.AddSlot(this.gameObject);
+        weaponManager.AddWeapon(this.gameObject, fpsCam.transform);
+
+    }
+
+    // Play the shooting sound effect
+    protected void PlayShootingSound()=> gunAudio.PlayOneShot(gunSounds[0]);
+  
+
+    //Responsable for doing the math for the weapon Spread
+    protected virtual Vector3 WeaponSpread()
+    {
+        float recoilSpreadFactor;
+        // Else, if there's only 1 pellet per bullet shot, spread it according to the current gun recoil
+        float currentRecoil = 360.0f - this.transform.localEulerAngles.x;
+        if (currentRecoil >= 360f) currentRecoil = 0.0f;
+        recoilSpreadFactor = currentRecoil;
+
+        float pelletSpreadRadius = Random.Range(0.0f, recoilSpreadFactor / 400.0f) * pelletSpreadRadiusMultiplier;  // Pellet spread radius increases with the current weapon X rotation, due to recoil
+        Vector3 pelletSpreadAngle = this.transform.TransformDirection(Random.insideUnitCircle.normalized);  // TransformDirection from local space to world space | .normalized turns it into .onUnitCircle
+        Vector3 pelletDirection = this.transform.forward - (this.transform.forward.y - fpsCam.transform.forward.y) * 0.5f * Vector3.up + pelletSpreadAngle * pelletSpreadRadius;
+
+        return pelletDirection;
+    }
+
+    
+    protected float WeaponForceCalculation(RaycastHit hit) =>  1 - hit.distance / gunRange;
+   
+
+
+    protected void DoDamage(Enemy enemy, float impactPercentageWithDistance)
+    {
+        // If there was an Enemy script attached
+        if (enemy != null && !enemy.isDying())
+        {
+            // Call the damage function of the Enemy script, passing in our pelletDamage
+            enemy.Damage(BulletDamage * impactPercentageWithDistance);
+        }
+    }
+
+
+    protected void ApplyWeaponForce(RaycastHit hit, float impactPercentageWithDistance)
+    {
+        // Check if the object we hit has a rigidbody attached
+        if (hit.rigidbody != null)
+        {
+            // Add force to the rigidbody we hit, in the direction from which it was hit
+            hit.rigidbody.AddForce(-hit.normal * hitForce * impactPercentageWithDistance);
+        }
+    }
+}
+
---
+++ Assets/Scripts/Gun/WeaponHierarchy/WeaponManager.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class WeaponManager : MonoBehaviour
+{
+    // Value obtained by hand testing best position
+    private readonly Vector3 defaultOffSet = new Vector3(0.47f, -0.12f, 0.7f);
+
+    [SerializeField] private List<WeaponInfo> ownedGuns; // SERIALIZED FOR DEBUGGING
+
+    [SerializeField] private WeaponInfo equipedGun;    // SERIALIZED FOR DEBUGGING
+
+    [SerializeField] private GameObject previousGun;
+
+    // Input keycodes 
+    private readonly KeyCode shootKeycode = KeyCode.Mouse0;
+    private readonly KeyCode reloadKeycode = KeyCode.R;
+    
+    private void Awake()
+    {
+        ownedGuns = new List<WeaponInfo>();
+    }
+
+    private void Update()
+    {
+        WeaponSelectionInput();
+
+        WeaponActionInput();
+    }
+
+    private void WeaponSelectionInput()
+    {
+        // Check for input if u pressed any of the weapons associated keys
+        foreach (WeaponInfo weaponInfo in ownedGuns)
+        {
+            if (Input.GetKeyDown(weaponInfo.weapon.weaponKeyCode))
+            {
+                // If so switch for respective weapon
+                SwitchWeapon(weaponInfo);
+            }
+        }
+    }
+
+    private void WeaponActionInput()
+    {
+        // Guard clause
+        if (equipedGun == null) return;
+
+
+        if (Input.GetKeyDown(shootKeycode))
+        {
+            equipedGun.weapon.Attacking();
+        }
+
+        if (Input.GetKeyDown(reloadKeycode))
+        {
+            equipedGun.weapon.ChangeMagazine(equipedGun.remainingMagazines);
+            if (equipedGun.remainingMagazines > 0)
+                equipedGun.remainingMagazines--;
+        }
+    }
+
+
+    private void SwitchWeapon(WeaponInfo weaponToEquip)
+    {
+        GameObject weaponToEquipObject = weaponToEquip.weapon.gameObject;
+
+        try
+        {
+            // Gives exception in the first weapon picked up
+            previousGun.gameObject.SetActive(false);
+        }
+        catch (Exception)
+        {
+            // Exception happening in the first weapon picked up
+        }
+        finally
+        {
+            // Equips new weapon and deactivates previous equiped one
+            weaponToEquipObject.SetActive(true);
+            equipedGun = weaponToEquip;
+            previousGun = weaponToEquipObject;
+        }
+        
+       
+    }
+
+    public void AddWeapon(GameObject newWeapon, Transform parentCamera)
+    {
+        WeaponInfo weaponInfo = new WeaponInfo
+        {
+            remainingMagazines = 1,
+            weapon = newWeapon.GetComponent<Weapon>(),
+        };
+
+        ownedGuns.Add(weaponInfo);
+        
+        // Sets position and rotation to parent
+        newWeapon.transform.position = Vector3.zero;
+        newWeapon.transform.SetParent(parentCamera);
+        newWeapon.transform.localRotation = Quaternion.identity;
+        newWeapon.transform.localPosition = defaultOffSet;
+        newWeapon.transform.LookAt(parentCamera.position + (parentCamera.forward * 50f));
+        
+        // Initiates default position and rotation 
+        weaponInfo.weapon.OnPickUpDefaultInit(Quaternion.identity, defaultOffSet);
+        
+        //Switches for new weapon
+        equipedGun = weaponInfo;
+        SwitchWeapon(weaponInfo);
+    }
+
+    
+}
