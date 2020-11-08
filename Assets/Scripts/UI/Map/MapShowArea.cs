acd6dfaee9bc908285eff93002837ad50bd6e98e
oceanncs <59238106+oceanncs@users.noreply.github.com>
2020-11-08 17:04:15 +0000

    Dynamic Map For Sprint

---
+++ Assets/Scripts/UI/Map/MapShowArea.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapShowArea : MonoBehaviour
+{
+	[SerializeField] GameObject mapArea;
+
+	void OnTriggerEnter(Collider other)
+	{
+		mapArea.SetActive(true);
+	}
+}
