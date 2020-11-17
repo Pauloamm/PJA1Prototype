fa3d6b29858af157d0188ed2aca9c3af36d28bbc
oceanncs <59238106+oceanncs@users.noreply.github.com>
2020-11-17 17:24:36 +0000

    Inventory Revamp
    New Menu system
    New inventory system
    New map system
    New Health system with gif

--- Assets/Scripts/Inventory/SlotSelection.cs
+++ Assets/Scripts/Inventory/SlotSelection.cs
@@ -10,21 +10,35 @@ public class SlotSelection : MonoBehaviour
 
     DisplayPanel displayPanel;
 
-    void Awake()
+
+	//LUis
+	//Image itemInfo;
+	GameObject itemInfo;
+	//LUis
+
+	void Awake()
     {
         displayPanel = GameObject.Find("DisplayPanel").GetComponent<DisplayPanel>();
+		itemInfo = GameObject.Find("ItemInfo");
     }
 
     public void OnPointerEnter()
     {
         displayPanel.OnMouseClick += GameObjectReturns;
-        Debug.Log("entrou");
+
+		//itemInfo.sprite = this.gameObject.GetComponent<Slot>().icon;
+		itemInfo.GetComponent<Mask>().enabled = false;
+		itemInfo.GetComponent<Image>().sprite = this.gameObject.GetComponent<Slot>().icon;
+
+		Debug.Log("entrou");
     }
     public void OnPointerExit()
     {
         displayPanel.OnMouseClick -= GameObjectReturns;
-        Debug.Log("saiu");
 
+		itemInfo.GetComponent<Mask>().enabled = true;
+
+		Debug.Log("saiu");
     }
 
     GameObject GameObjectReturns() => this.gameObject;
--- Assets/Scripts/Inventory/UiStateManager.cs
+++ Assets/Scripts/Inventory/UiStateManager.cs
@@ -30,6 +30,4 @@ public class UiStateManager : MonoBehaviour
             InventoryClosed?.Invoke();
         }
     }
-    
-   
 }
